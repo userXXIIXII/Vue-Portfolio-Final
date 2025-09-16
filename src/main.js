41fbb10007c3a396fd923b1da6f4e43910aa30f5
Aarin Deb <aarindeb2003@gmail.com>
2025-09-16 21:56:08 +0200

    Création des chemins de components et du router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+import { createRouter, createWebHistory } from 'vue-router'
+import Intro from './components/Intro.vue'        
+import About from './components/About.vue'
+import Experiences from './components/Experiences.vue'
+import Projects from './components/Projects.vue'
+import Contact from './components/Contact.vue'
+
+const routes = [
+  { path: '/', component: Intro },     
+  { path: '/about', component: About }, 
+  { path: '/experiences', component: Experiences},   
+  { path: '/projects', component: Projects }, 
+  { path: '/contact', component: Contact }
+
+]
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+createApp(App)
+  .use(router) 
+  .mount('#app')
