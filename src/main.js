158a2f4c91725db3faf4e7b6debb31894f3d79cf
Aarin Deb <aarindeb2003@gmail.com>
2025-09-17 20:18:10 +0200

    Suppression du contenu de base du composant style css

--- src/main.js
+++ src/main.js
@@ -5,15 +5,15 @@ import './style.css'
 import { createRouter, createWebHistory } from 'vue-router'
 import Intro from './components/Intro.vue'        
 import About from './components/About.vue'
-import Experiences from './components/Experiences.vue'
-import Projects from './components/Projects.vue'
+import Competence from './components/Competence.vue'
+import Projets from './components/Projets.vue'
 import Contact from './components/Contact.vue'
 
 const routes = [
   { path: '/', component: Intro },     
   { path: '/about', component: About }, 
-  { path: '/experiences', component: Experiences},   
-  { path: '/projects', component: Projects }, 
+  { path: '/competence', component: Competence},   
+  { path: '/projets', component: Projets }, 
   { path: '/contact', component: Contact }
 
 ]
